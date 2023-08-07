63c4a70461b0fec36856244a283362ee3907113f
letantai3001180588 <54881497+letantai3001180588@users.noreply.github.com>
2023-08-07 23:53:36 +0700

    update

--- ecomere_app/settings.py
+++ ecomere_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lt1suk%_)y&e18p*&fmjli)qp1c*5z+j19j2sqfg95_y5p)vwd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -130,8 +130,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-# STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
