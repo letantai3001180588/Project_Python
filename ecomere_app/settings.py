dbdaf196b3e6ee8be844cd80b56254be481199e9
letantai3001180588 <54881497+letantai3001180588@users.noreply.github.com>
2023-08-07 23:48:34 +0700

    update

--- ecomere_app/settings.py
+++ ecomere_app/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
