2768f19b5b8c7de3f8405b51ebba053ccd95d074
letantai3001180588 <54881497+letantai3001180588@users.noreply.github.com>
2023-08-07 22:39:57 +0700

    update

--- ecomere_app/settings.py
+++ ecomere_app/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 # STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 # Default primary key field type
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'/app/static')
 ]
 #
 # MEDIA_URL = '/images'
